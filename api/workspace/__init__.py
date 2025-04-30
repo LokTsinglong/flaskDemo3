4fd90af2cd3f55d7343e248743340bd7f0139b0c
Lok <1027408923@qq.com>
2025-04-30 16:32:28 +0800

    April 30 ocr original

--- api/workspace/__init__.py
+++ api/workspace/__init__.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint,session,redirect,url_for,render_template
 
 
 workspace_main = Blueprint('workspace_main', __name__)
+
+# 这边路由一定要改下，否则会跳到index.html也就是登录的那个界面
+@workspace_main.route('/workspace_main')
+def index():
+    if 'user' not in session:
+        
+        return redirect(url_for('auth.login'))
+    return render_template("dashboard.html")
 
 #延迟导入，否则可能会有(most likely due to a circular import
 from .workspace1 import workspace1_api
